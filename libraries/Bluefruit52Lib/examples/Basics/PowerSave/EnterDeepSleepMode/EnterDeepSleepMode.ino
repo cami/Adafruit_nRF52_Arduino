31eeb85b4c0eb0ef095ead6e1ae6987fd227def1
NaotoFujihiro <naoto.fujihiro@gmail.com>
2019-08-18 20:15:24 +0900

    Clean the sample for entering deep sleep mode.

--- libraries/Bluefruit52Lib/examples/Basics/PowerSave/EnterDeepSleepMode/EnterDeepSleepMode.ino
+++ libraries/Bluefruit52Lib/examples/Basics/PowerSave/EnterDeepSleepMode/EnterDeepSleepMode.ino
@@ -2,61 +2,41 @@
 #include "nrf_power.h"
 #include <NectisCellular.h>
 
-////////////////////////////////////////////////////////////////////////////////
-// Defines
-
-#define BOOT_INTERVAL_SECOND   (30)  // [sec.]
 #define SLAVE_SELECT_PIN      PIN_SPI_CS
-
-////////////////////////////////////////////////////////////////////////////////
-// Global variables
 
 NectisCellular Nectis;
 
-////////////////////////////////////////////////////////////////////////////////
-// setup and loop
-
-
-// Using the power-fail comparator (POF) is optional. When enabled, it can provide the CPU an early warning
-// of an impending power supply failure.
-// p.65
-
+// cf. nRF52840 product specification p.69
+// Note: Watchdog reset is not available in System OFF.
 
 
 void setup() {
   // Wake up from System_Off sleep mode.
-  NRF_UART0->ENABLE = 1;  //disable all UART instance
-  NRF_UARTE0->ENABLE = 1;
-  NRF_UARTE1->ENABLE = 1;
-  NRF_SAADC->ENABLE = 1; //disable ADC
-  NRF_PWM0->ENABLE = 1; //disable all pwm instance
-  NRF_PWM1->ENABLE = 1;
-  NRF_PWM2->ENABLE = 1;
-  NRF_TWIM1->ENABLE = 1; //disable TWI Master
-  NRF_TWIS1->ENABLE = 1; //disable TWI Slave
-
-  NRF_SPI0->ENABLE = 1; //disable SPI
-  NRF_SPI1->ENABLE = 1; //disable SPI
-  NRF_SPI2->ENABLE = 1; //disable SPI
-  NRF_QSPI->ENABLE = 1; //disable QSPI
+//  NRF_UART0->ENABLE = 1;  //disable all UART instance
+//  NRF_UARTE0->ENABLE = 1;
+//  NRF_UARTE1->ENABLE = 1;
+//  NRF_SAADC->ENABLE = 1; //disable ADC
+//  NRF_PWM0->ENABLE = 1; //disable all pwm instance
+//  NRF_PWM1->ENABLE = 1;
+//  NRF_PWM2->ENABLE = 1;
+//  NRF_TWIM1->ENABLE = 1; //disable TWI Master
+//  NRF_TWIS1->ENABLE = 1; //disable TWI Slave
+//
+//  NRF_SPI0->ENABLE = 1; //disable SPI
+//  NRF_SPI1->ENABLE = 1; //disable SPI
+//  NRF_SPI2->ENABLE = 1; //disable SPI
+//  NRF_QSPI->ENABLE = 1; //disable QSPI
 
 //  NRF_POWER->SYSTEMOFF = (0UL);
 //  NRF_POWER->EVENTS_SLEEPEXIT = (1UL);
 
 //  NRF_POWER->RAM[0].POWER = (1UL);
 
-
-
-
-
   delay(4000);
 
   Serial.begin(115200);
   Serial.println("");
   Serial.println("--- START ---------------------------------------------------");
-
-  ////////////////////////////////////////
-  // Low-level initialize
 
   Serial.println("### I/O Initialize.");
   Nectis.Init();
@@ -67,12 +47,11 @@ void setup() {
   delay(100);
 
   Serial.println("### Power supply ON.");
-  //  Make sure that the MODULE_PWR_PIN is set to HIGH.
+  // Make sure that the MODULE_PWR_PIN is set to HIGH.
   Nectis.PowerSupplyGrove(true);
 
-  // set the slaveSelectPin as an output:
+  // Use SPI between flash ROM in order to put it into deep sleep mode.
   pinMode(SLAVE_SELECT_PIN, OUTPUT);
-  // initialize SPI:
   SPI.begin();
 
   Nectis.Bg96Begin();
@@ -80,6 +59,7 @@ void setup() {
   delay(100);
 
 
+  // When the MCU is in deep sleep mode, LEDs are turned off.
   pinMode(LED_BLUE, OUTPUT);
   pinMode(LED_RED, OUTPUT);
 
@@ -97,114 +77,97 @@ void setup() {
   digitalWrite(LED_RED, HIGH);
 
 
-
-
-
-
-//  for (int i = 0; i < 16; i++) {
-//    Serial.print("NRF_POWER->RAM ");
-//    Serial.print(i);
-//    Serial.print(" ");
-//    Serial.println(NRF_POWER->RAM[i].POWER);
-//    delay(100);
-//  }
-
-
-
-
-
-//  for (int i = 0; i < 9;  i++) {
-//    NRF_POWER->RAM[i].POWER = 0;
-//  }
-
-  Serial.println("Enter deep power-down mode");
-  // take the SS pin low to select the chip:
+  // Put flash ROM into deep sleep mode.
+  Serial.println("Put flash ROM into deep sleep mode");
   digitalWrite(SLAVE_SELECT_PIN, LOW);
-  //  send value via SPI:
   SPI.transfer(0xB9);
-  // take the SS pin high to de-select the chip:
   digitalWrite(SLAVE_SELECT_PIN, HIGH);
 
   // Turn off BG96 casting AT commands.
-  Serial.println("BG96TurnOff");
+  Serial.println("Turn off BG96 casting AT commands.");
   Nectis.Bg96TurnOff();
   delay(100);
-  // End Uart between nRF52840 and BG96.
-  Serial.println("Bg96End");
+  // Disable Uart between nRF52840 and BG96.
+  Serial.println("End Uart between nRF52840 and BG96.");
   Nectis.Bg96End();
   delay(100);
 
-//  Serial.println("### Power supply OFF.");
-//  digitalWrite(GROVE_VCCB_PIN, LOW);
-//  delay(100);
-//
-//  Serial.println("### Power supply cellular OFF.");
-//  digitalWrite(MODULE_PWRKEY_PIN, LOW);
-//  digitalWrite(MODULE_PWR_PIN, LOW);
-//  delay(100);
+  Serial.println("### Power supply OFF.");
+  digitalWrite(GROVE_VCCB_PIN, LOW);
+  delay(100);
+
+  Serial.println("### Power supply cellular OFF.");
+  digitalWrite(MODULE_PWRKEY_PIN, LOW);
+  digitalWrite(MODULE_PWR_PIN, LOW);
+  delay(100);
+
+  Serial.println("Enter deep sleep mode.");
 
   digitalWrite(LED_BLUE, LOW);
   digitalWrite(LED_RED, LOW);
 
+  // Disable the peripherals.
+//  NRF_UART0->ENABLE = UART_ENABLE_ENABLE_Disabled << UART_ENABLE_ENABLE_Pos;
+//  NRF_UARTE0->ENABLE = UART_ENABLE_ENABLE_Disabled << UART_ENABLE_ENABLE_Pos;
+//  NRF_UARTE1->ENABLE = UART_ENABLE_ENABLE_Disabled << UART_ENABLE_ENABLE_Pos;
+//
+//  NRF_SAADC->ENABLE = SAADC_ENABLE_ENABLE_Disabled << SAADC_ENABLE_ENABLE_Pos;
+//
+//  NRF_PWM0->ENABLE = PWM_ENABLE_ENABLE_Disabled << PWM_ENABLE_ENABLE_Pos;
+//  NRF_PWM1->ENABLE = PWM_ENABLE_ENABLE_Disabled << PWM_ENABLE_ENABLE_Pos;
+//  NRF_PWM2->ENABLE = PWM_ENABLE_ENABLE_Disabled << PWM_ENABLE_ENABLE_Pos;
+//
+//  NRF_TWI0->ENABLE = TWI_ENABLE_ENABLE_Disabled << TWI_ENABLE_ENABLE_Pos;
+//  NRF_TWI1->ENABLE = TWI_ENABLE_ENABLE_Disabled << TWI_ENABLE_ENABLE_Pos;
+//
+//  NRF_SPI0->ENABLE = SPI_ENABLE_ENABLE_Disabled << SPI_ENABLE_ENABLE_Pos;
+//  NRF_SPI1->ENABLE = SPI_ENABLE_ENABLE_Disabled << SPI_ENABLE_ENABLE_Pos;
+//  NRF_SPI2->ENABLE = SPI_ENABLE_ENABLE_Disabled << SPI_ENABLE_ENABLE_Pos;
+//
+//  NRF_QSPI->ENABLE = QSPI_ENABLE_ENABLE_Disabled << QSPI_ENABLE_ENABLE_Pos;
+//
+//  NRF_NFCT->TASKS_DISABLE = 0x1UL << NFCT_TASKS_DISABLE_TASKS_DISABLE_Pos;
 
 
-
-  // nrf_power_task_trigger(NRF_POWER_TASK_LOWPWR);
-  // nrf_power_system_off();
-
-  // Disable the pins.
   NRF_UART0->ENABLE = 0;  //disable all UART instance
   NRF_UARTE0->ENABLE = 0;
   NRF_UARTE1->ENABLE = 0;
+
   NRF_SAADC->ENABLE = 0; //disable ADC
+
   NRF_PWM0->ENABLE = 0; //disable all pwm instance
   NRF_PWM1->ENABLE = 0;
   NRF_PWM2->ENABLE = 0;
-  NRF_TWIM1->ENABLE = 0; //disable TWI Master
-  NRF_TWIS1->ENABLE = 0; //disable TWI Slave
 
-  NRF_SPI0->ENABLE = 0; //disable SPI
-  NRF_SPI1->ENABLE = 0; //disable SPI
-  NRF_SPI2->ENABLE = 0; //disable SPI
+  NRF_TWI0->ENABLE = 0; //disable TWI
+  NRF_TWI1->ENABLE = 0;
+
+  NRF_SPI0->ENABLE = 0; //disable all SPI
+  NRF_SPI1->ENABLE = 0;
+  NRF_SPI2->ENABLE = 0;
 
   NRF_QSPI->ENABLE = 0; //disable QSPI
-  NRF_QSPI->DPMDUR = 0; //enter deep sleep mode
 
   NRF_NFCT->TASKS_DISABLE = 1; //disable NFC, confirm this is the right way
 
   delay(1000);
 
-  NRF_POWER->RAM[4].POWER = (0UL);
 
-  // Enter System_Off sleep mode.
-  NRF_POWER->SYSTEMOFF = (1UL);
 
-  NRF_POWER->EVENTS_SLEEPENTER = (1UL);
+//  NRF_POWER->RAM[4].POWER = (0UL);
+
+  // Enter System-Off deep sleep mode.
+  NRF_POWER->SYSTEMOFF = POWER_SYSTEMOFF_SYSTEMOFF_Enter << POWER_SYSTEMOFF_SYSTEMOFF_Pos;
+  // nrf_power_system_off();
+
+  // CPU entered WFI/WFE sleep.
+  NRF_POWER->EVENTS_SLEEPENTER = 0x1UL << POWER_EVENTS_SLEEPENTER_EVENTS_SLEEPENTER_Pos;
+
+//  NRF_POWER->SYSTEMOFF = (1UL);
+//
+//  NRF_POWER->EVENTS_SLEEPENTER = (1UL);
 }
 
 void loop() {
-//  uint8_t val;
-//  Rtc.EepromRead(0, &val, sizeof(val));
-//  Serial.print("EEPROM value is ");
-//  Serial.println(val);
-//
-//  val++;
-//  Rtc.EepromWrite(0, &val, sizeof(val));
-//
-//  Rtc.SetWakeupPeriod(BOOT_INTERVAL_SECOND);
-//
-//  Serial.println("Shutdown.");
-//  Rtc.Shutdown();
-//  delay(1000);
-//
-//  NRF_POWER->RAM[0].POWER = (0UL);
-//  NRF_POWER->SYSTEMOFF = (1UL);
-//  NRF_POWER->EVENTS_SLEEPENTER = (1UL);
-//
-//  while (1) {
-//    Serial.println("RTC sleep");
-//    delay(1000);
-//  }
-}
 
-////////////////////////////////////////////////////////////////////////////////
+}
