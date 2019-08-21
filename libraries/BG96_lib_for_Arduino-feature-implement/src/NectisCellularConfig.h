20b12f0cfc7f60f656f79f82fda326ebf3be5538
NaotoFujihiro <naoto.fujihiro@gmail.com>
2019-08-21 13:04:48 +0900

    Enable to DEBUG_PRINT when you select Debug Level 2 or higher on ArduinoIDE

--- libraries/BG96_lib_for_Arduino-feature-implement/src/NectisCellularConfig.h
+++ libraries/BG96_lib_for_Arduino-feature-implement/src/NectisCellularConfig.h
@@ -12,4 +12,8 @@
 #undef max /* FIXME workaround: https://stackoverflow.com/questions/41093090/esp8266-error-macro-min-passed-3-arguments-but-takes-just-2 */
 #undef min /* FIXME workaround: https://stackoverflow.com/questions/41093090/esp8266-error-macro-min-passed-3-arguments-but-takes-just-2 */
 
-#define NECTIS_DEBUG
+// CFG_DEBUG == 2 => (Full Debug)
+// CFG_DEBUG == 3 => (Segger SystemView)
+#if CFG_DEBUG >= 2
+  #define NECTIS_DEBUG
+#endif //CFG_DEBUG >= 2
