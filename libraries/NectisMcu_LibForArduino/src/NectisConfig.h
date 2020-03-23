1c5e6eb173763f220d39f0f4d497d6fa94b56a22
NaotoFujihiro <naoto.fujihiro@gmail.com>
2020-03-23 14:04:08 +0900

    Bugfix: enable to define NECTIS_DEBUG if needed

--- libraries/NectisMcu_LibForArduino/src/NectisConfig.h
+++ libraries/NectisMcu_LibForArduino/src/NectisConfig.h
@@ -6,10 +6,6 @@
 // CFG_DEBUG == 2 => (Full Debug)
 // CFG_DEBUG == 3 => (Segger SystemView)
 
-#if CFG_DEBUG == 0
-
-#elif CFG_DEBUG == 1
-
-#elif CFG_DEBUG >= 2
+#if CFG_DEBUG >= 2
   #define NECTIS_DEBUG
 #endif //CFG_DEBUG
